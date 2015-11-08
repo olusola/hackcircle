abfaa9b86f3af94c11771372c8908d4ca8669518
olusola <olusola2509@gmail.com>
2015-11-08 10:56:10 +0000

    8080

--- server.js
+++ server.js
@@ -43,7 +43,7 @@ app.get('/', function(req, res) {
 
 // PORT
 
-var server = app.listen(3000
+var server = app.listen(8080
 , function () {
   var host = server.address().address;
   var port = server.address().port;
